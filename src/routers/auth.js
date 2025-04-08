98283e8c4b0b5a1e2b028cec23e6a9c2a4d35e6b
Alena-Solo <bazarkina.alena2020@gmail.com>
2025-04-08 14:53:36 +0200

    swagger

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,7 +1,14 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
-import { registerUserController } from '../controllers/auth.js';
+import {
+  loginUserSchema,
+  loginWithGoogleOAuthSchema,
+  registerUserSchema,
+} from '../validation/auth.js';
+import {
+  loginWithGoogleController,
+  registerUserController,
+} from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import {
   loginUserController,
@@ -16,6 +23,8 @@ import {
   requestResetEmailController,
   resetPasswordController,
 } from '../controllers/auth.js';
+
+import { getGoogleOAuthUrlController } from '../controllers/auth.js';
 
 const router = Router();
 
@@ -45,6 +54,14 @@ router.post(
   '/reset-password',
   validateBody(resetPasswordSchema),
   ctrlWrapper(resetPasswordController),
+);
+
+router.get('/get-oauth-url', ctrlWrapper(getGoogleOAuthUrlController));
+
+router.post(
+  '/confirm-oauth',
+  validateBody(loginWithGoogleOAuthSchema),
+  ctrlWrapper(loginWithGoogleController),
 );
 
 export default router;
